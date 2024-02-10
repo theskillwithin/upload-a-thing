d6000035165438e8f6dab39ac42a9ee5ad8cd6b4
Austin Peterson <austinppeterson@gmail.com>
2024-02-09 20:53:34 -0700

    tailwind upload config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -17,4 +19,4 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
+export default withUt(config);
